d3f0a1b2acebc822b2b9147c8863db87fb64de9e
Baptiste Cecconi <baptiste.cecconi@obspm.fr>
2021-05-20 18:54:16 +0200

    fixed NASA API response

--- tests/test_api.py
+++ tests/test_api.py
@@ -130,8 +130,8 @@ def test_api_metadata():
     assert API['documentation'] == \
         'https://wgc2.jpl.nasa.gov:8443/webgeocalc/documents/api-info.html'
     assert API['contact'] == 'Boris Semenov <Boris.Semenov@jpl.nasa.gov>'
-    assert API['version'] == '2.2.2'
-    assert API['build_id'] == '5004 N66 23-JUL-2020'
+    assert API['version'] == '2.2.5'
+    assert API['build_id'] == '5089 N66 01-FEB-2021'
 
     with pytest.raises(KeyError):
         _ = API['foo']
